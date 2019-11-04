961b470e14f6bc0a684a296a1e44c9c39f40a147
huanshenyi <txy1226052@gmail.com>
2019-11-05 00:09:18 +0900

    seleniumの関数を試す

--- appium-test/rikunabi.py
+++ appium-test/rikunabi.py
@@ -1,6 +1,9 @@
 __author__ = "ハリネズミ"
-from appium import webdriver
 import time
+from appium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 cap = {
   "platformName": "Android",
@@ -19,8 +22,18 @@ driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widg
                              ".widget.DrawerLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager"
                              "/android.widget.RelativeLayout/android.view.View/android.widget.ScrollView/android.widget"
                              ".LinearLayout/android.widget.RelativeLayout[2]/android.widget.TextView").click()
+locator = (By.XPATH,
+                                   "/hierarchy/android.widget.FrameLayout/android."
+                                   "widget.LinearLayout/android.widget."
+                                   "FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support."
+                                   "v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout"
+                                   "/android.support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget."
+                                   "ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget"
+                                   ".EditText")
 
-time.sleep(2)
+WebDriverWait(driver, 3).until(
+    EC.presence_of_element_located(locator)
+)
 
 driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget."
                              "FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4."
