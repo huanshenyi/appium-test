c8cf3fa54af9ab8469e843d762025ed1d9eec505
huanshenyi <txy1226052@gmail.com>
2019-11-05 22:02:56 +0900

    rikunabi-スクロール

--- appium-test/rikunabi.py
+++ appium-test/rikunabi.py
@@ -5,24 +5,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-cap = {
-  "platformName": "Android",
-  "platformVersion": "5.1.1",
-  "deviceName": "127.0.0.1:62001",
-  "appPackage": "jp.co.recruit.android.rikunabi.twenty",
-  "appActivity": "jp.co.recruit.android.rikunabi.twenty.activity.home.HomeActivity",
-  "noReset": True
-}
+# cap = {
+#   "platformName": "Android",
+#   "platformVersion": "5.1.1",
+#   "deviceName": "127.0.0.1:62001",
+#   "appPackage": "jp.co.recruit.android.rikunabi.twenty",
+#   "appActivity": "jp.co.recruit.android.rikunabi.twenty.activity.home.HomeActivity",
+#   "noReset": True
+# }
 
 # appiumに接続
-driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", cap)
+# driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", cap)
 
-driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget."
-                             "FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4"
-                             ".widget.DrawerLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager"
-                             "/android.widget.RelativeLayout/android.view.View/android.widget.ScrollView/android.widget"
-                             ".LinearLayout/android.widget.RelativeLayout[2]/android.widget.TextView").click()
-locator = (By.XPATH,
+
+class Rikunabi(object):
+    def __init__(self):
+        self.cap = {
+                      "platformName": "Android",
+                      "platformVersion": "5.1.1",
+                      "deviceName": "127.0.0.1:62001",
+                      "appPackage": "jp.co.recruit.android.rikunabi.twenty",
+                      "appActivity": "jp.co.recruit.android.rikunabi.twenty.activity.home.HomeActivity",
+                      "noReset": True
+                    }
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", self.cap)
+
+    def do_this(self):
+        self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget."
+                                     "FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4"
+                                     ".widget.DrawerLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager"
+                                     "/android.widget.RelativeLayout/android.view.View/android.widget.ScrollView/android.widget"
+                                     ".LinearLayout/android.widget.RelativeLayout[2]/android.widget.TextView").click()
+        locator = (By.XPATH,
                                    "/hierarchy/android.widget.FrameLayout/android."
                                    "widget.LinearLayout/android.widget."
                                    "FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support."
@@ -31,17 +45,37 @@ locator = (By.XPATH,
                                    "ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget"
                                    ".EditText")
 
-WebDriverWait(driver, 3).until(
-    EC.presence_of_element_located(locator)
-)
+        WebDriverWait(self.driver, 3).until(EC.presence_of_element_located(locator))
 
-driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget."
+        self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget."
                              "FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4."
                              "widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android."
                              "support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget.ScrollView/android"
-                             ".widget.LinearLayout/android.widget.RelativeLayout/android.widget.EditText").send_keys("ベルフェイス")
-time.sleep(1)
-driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.Button[2]").click()
+                             ".widget.LinearLayout/android.widget.RelativeLayout/android.widget.EditText").send_keys("リクルート")
+        time.sleep(1)
+        self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.Button[2]").click()
+        l = self.get_size()
+        # 最初の位置
+        x1 = int(l[0]*0.5)
+        # どこからスクロール
+        y1 = int(l[1]*0.75)
+        # 何処で止まる
+        y2 = int(l[1]*0.25)
+        time.sleep(2)
+        # スクロール操作
+        while True:
+            self.driver.swipe(x1, y1, x1, y2)
+            time.sleep(0.5)
 
+    # スクロール用
+    def get_size(self):
+        x = self.driver.get_window_size()["width"]
+        y = self.driver.get_window_size()["height"]
+        return (x, y)
+
+
+if __name__ == "__main__":
+    r = Rikunabi()
+    r.do_this()
 
 
